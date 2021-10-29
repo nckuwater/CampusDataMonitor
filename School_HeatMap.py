1e93d538b090f5ec9da61b808c7481dd7405be4c
LCL-cdsak <patrick881212@gmail.com>
2021-10-29 23:46:49 +0800

    Peaceful

--- School_HeatMap.py
+++ School_HeatMap.py
@@ -17,7 +17,9 @@ from PyQt5.QtWidgets import QLabel
 import pandas as pd
 import pprint
 import Take_data
+import campus_data_monitor
 cdm =Take_data.CampusDataMonitor()
+cdm2 = campus_data_monitor.CampusDataMonitor()
 layout=QVBoxLayout()
 class Window(QDialog):
     def __init__(self):
@@ -26,41 +28,38 @@ class Window(QDialog):
         self.resize(640, 480)
         self.setLayout(layout)
         data =Take_data.Take_Data_Now(cdm)
+        data2 = campus_data_monitor.Take_Data_Now(cdm2)
         fmap = folium.Map(location=[22.996363580424514, 120.21903238440177], zoom_start=17)
         HeatMap(data,min_opacity=0.4,blur=50,radius=50).add_to(fmap)
         fmap.save("test.html")
         #folium.Marker(location=[22.99891110398634, 120.21723168980603],popup='<i>雲平大樓：<i>').add_to(fmap)
 
 
-        html =   '''<font face="微軟正黑體"><font size="4">雲平大樓</font><br>
-                    <font face="Arial"><font size="3">2nd line<br>
-                    3rd line'''
-        iframe = folium.IFrame(html,width=100,height=50)
+        html =   '''<font face="微軟正黑體"><font size="4">雲平大樓:</font>
+                    ''' + str(data2[0][1])
+        iframe = folium.IFrame(html,width=150,height=40)
         popup = folium.Popup(iframe,show=True,max_width=200,max_height=200)
-        m1 = folium.Marker(location=[22.99891110398634, 120.21723168980603],popup=popup)
+        m0 = folium.Marker(location=[22.99891110398634, 120.21723168980603],popup=popup)
 
-        html =   '''<font face="微軟正黑體"><font size="4">資工系館</font><br>
-                    <font face="Arial"><font size="3">2nd line<br>
-                    3rd line'''
-        iframe = folium.IFrame(html,width=100,height=50)
+        html =   '''<font face="微軟正黑體"><font size="4">資工系館:</font>
+                    ''' + str(data2[1][1])
+        iframe = folium.IFrame(html,width=150,height=40)
         popup = folium.Popup(iframe,show=True,max_width=200,max_height=200)
-        m2 = folium.Marker(location=[22.99726633629006, 120.2212997328783],popup=popup)
-        html =   '''<font face="微軟正黑體"><font size="4">計網中心</font><br>
-                    <font face="Arial"><font size="3">2nd line<br>
-                    3rd line'''
-        iframe = folium.IFrame(html,width=100,height=50)
+        m1 = folium.Marker(location=[22.99726633629006, 120.2212997328783],popup=popup)
+        html =   '''<font face="微軟正黑體"><font size="4">修齊大樓:</font>
+                    ''' + str(data2[2][1])
+        iframe = folium.IFrame(html,width=150,height=40)
+        popup = folium.Popup(iframe,show=True,max_width=200,max_height=200)
+        m2 = folium.Marker(location=[23.00085204740406, 120.21786248420196],popup=popup)
+        html =   '''<font face="微軟正黑體"><font size="4">計網中心:</font>
+                    ''' + str(data2[3][1])
+        iframe = folium.IFrame(html,width=150,height=40)
         popup = folium.Popup(iframe,show=True,max_width=200,max_height=200)
         m3 = folium.Marker(location=[22.99811369711759, 120.2184468855774],popup=popup)
-        html =   '''<font face="微軟正黑體"><font size="4">修齊大樓</font><br>
-                    <font face="Arial"><font size="3">2nd line<br>
-                    3rd line'''
-        iframe = folium.IFrame(html,width=100,height=50)
-        popup = folium.Popup(iframe,show=True,max_width=200,max_height=200)
-        m4 = folium.Marker(location=[23.00085204740406, 120.21786248420196],popup=popup)
+        fmap.add_child(child=m0)
         fmap.add_child(child=m1)
         fmap.add_child(child=m2)
         fmap.add_child(child=m3)
-        fmap.add_child(child=m4)
 
 
         data2 = io.BytesIO()
@@ -78,36 +77,35 @@ class Window(QDialog):
         QtWidgets.QApplication.processEvents()
     def run(self):
         Data =Take_data.Take_Data_Now(cdm)
+        data2 = campus_data_monitor.Take_Data_Now(cdm2)
+        
         fmap = folium.Map(location=[22.996363580424514, 120.21903238440177], zoom_start=17)
-        html =   '''<font face="微軟正黑體"><font size="4">雲平大樓</font><br>
-                    <font face="Arial"><font size="3">2nd line<br>
-                    3rd line'''
-        iframe = folium.IFrame(html,width=100,height=50)
+        html =   '''<font face="微軟正黑體"><font size="4">雲平大樓:</font>
+                    ''' + str(data2[0][1])
+        iframe = folium.IFrame(html,width=150,height=40)
         popup = folium.Popup(iframe,show=True,max_width=200,max_height=200)
-        m1 = folium.Marker(location=[22.99891110398634, 120.21723168980603],popup=popup)
+        m0 = folium.Marker(location=[22.99891110398634, 120.21723168980603],popup=popup)
 
-        html =   '''<font face="微軟正黑體"><font size="4">資工系館</font><br>
-                    <font face="Arial"><font size="3">2nd line<br>
-                    3rd line'''
-        iframe = folium.IFrame(html,width=100,height=50)
+        html =   '''<font face="微軟正黑體"><font size="4">資工系館:</font>
+                    ''' + str(data2[1][1])
+        iframe = folium.IFrame(html,width=150,height=40)
         popup = folium.Popup(iframe,show=True,max_width=200,max_height=200)
-        m2 = folium.Marker(location=[22.99726633629006, 120.2212997328783],popup=popup)
-        html =   '''<font face="微軟正黑體"><font size="4">計網中心</font><br>
-                    <font face="Arial"><font size="3">2nd line<br>
-                    3rd line'''
-        iframe = folium.IFrame(html,width=100,height=50)
+        m1 = folium.Marker(location=[22.99726633629006, 120.2212997328783],popup=popup)
+        html =   '''<font face="微軟正黑體"><font size="4">修齊大樓:</font>
+                    ''' + str(data2[2][1])
+        iframe = folium.IFrame(html,width=150,height=40)
+        popup = folium.Popup(iframe,show=True,max_width=200,max_height=200)
+        m2 = folium.Marker(location=[23.00085204740406, 120.21786248420196],popup=popup)
+        html =   '''<font face="微軟正黑體"><font size="4">計網中心:</font>
+                    ''' + str(data2[3][1])
+        iframe = folium.IFrame(html,width=150,height=40)
         popup = folium.Popup(iframe,show=True,max_width=200,max_height=200)
         m3 = folium.Marker(location=[22.99811369711759, 120.2184468855774],popup=popup)
-        html =   '''<font face="微軟正黑體"><font size="4">修齊大樓</font><br>
-                    <font face="Arial"><font size="3">2nd line<br>
-                    3rd line'''
-        iframe = folium.IFrame(html,width=100,height=50)
-        popup = folium.Popup(iframe,show=True,max_width=200,max_height=200)
-        m4 = folium.Marker(location=[23.00085204740406, 120.21786248420196],popup=popup)
+        fmap.add_child(child=m0)
         fmap.add_child(child=m1)
         fmap.add_child(child=m2)
         fmap.add_child(child=m3)
-        fmap.add_child(child=m4)
+
         HeatMap(Data,min_opacity=0.4,blur=50,radius=50).add_to(fmap)
         data2 = io.BytesIO()
         fmap.save(data2,close_file=False)
--- campus_data_monitor.py
+++ campus_data_monitor.py
@@ -5,10 +5,9 @@ import pprint
 import time
 import datetime
 import sqlite3
-import matplotlib.pyplot as plt
 import concurrent.futures
-
-
+import pandas as pd
+ 
 class ThreadRequest:
     def load_url(self, url, timeout):
         return requests.get(url, timeout=timeout)
@@ -57,7 +56,7 @@ class CampusDataMonitor:
     """
 
     url_template = None
-    location_names = {}  # location: group, 門機名稱：管制區域
+    location_names = {}  # location: group
 
     def __init__(self, url_path='./config/ncku_api_url.txt',
                  location_names_path='./config/ogwebbasesetting_ogattlistctrl_ascx_211021.csv'):
@@ -66,57 +65,59 @@ class CampusDataMonitor:
 
         with open(location_names_path, 'r', encoding='utf8') as fr:
             location_name_lines = fr.read().split('\n')[1:]
-
-        i = 0
-        for line in location_name_lines:
-            # i += 1
-            # if i > 10:
-            #     break
-            if line == '':
-                continue
-            line = line.split(',')
-            self.location_names[line[0]] = line[1]
-
+        self.location_names = {'雲平大樓_B1駐警隊監控機房':'光復校區 雲平大樓',
+                                '雲平大樓_西棟B1梯間':'光復校區 雲平大樓',
+                                '雲平大樓_西棟B2梯間':'光復校區 雲平大樓',
+                                '雲平大樓_西棟電梯-右':	'光復校區 雲平大樓',
+                                '雲平大樓_西棟電梯-左':	'光復校區 雲平大樓',
+                                '雲平大樓_東棟電梯-右':	'光復校區 雲平大樓',
+                                '雲平大樓_東棟電梯-左':	'光復校區 雲平大樓',
+                                '雲平大樓B1B2廁所GCU':	'光復校區 雲平大樓',
+                                '雲平大樓B1男/女廁所':	'光復校區 雲平大樓',
+                                '雲平大樓B2女廁所':	'光復校區 雲平大樓',
+                                '雲平大樓B2男、無障礙廁所':	'光復校區 雲平大樓',
+                                '雲平大樓-電梯GCU':	'光復校區 雲平大樓',
+                                '資工系_B1安全門':'成功校區 資訊工程系',
+                                '資工系_B2安全門':'成功校區 資訊工程系',
+                                '資工系_北側門':'成功校區 資訊工程系',
+                                '資工系_南側門':'成功校區 資訊工程系',
+                                '資工系新館_1F東大門':'成功校區 資訊工程系',
+                                '資工系新館_1F南側門':'成功校區 資訊工程系',
+                                '資工系新館G1':'成功校區 資訊工程系',
+                                '文學院1F西側門':	'光復校區 文學院修齊大樓',
+                                '文學院2F前自動門':	'光復校區 文學院修齊大樓',
+                                '文學院2F後自動門':	'光復校區 文學院修齊大樓',
+                                '文學院B1電梯'	:'光復校區 文學院修齊大樓',
+                                '文學院B2電梯':'光復校區 文學院修齊大樓',
+                                '文學院GCU':'光復校區 文學院修齊大樓',
+                                '計網中心_1F自動門_出'	:'成功校區 計網中心',
+                                '計網中心_1F自動門_進'	:'成功校區 計網中心',
+                                '計網中心_2F（防疫）'	:'成功校區 計網中心',
+                                '計網中心_2F_75201'	:'成功校區 計網中心',
+                                '計網中心_2F_75209'	:'成功校區 計網中心',
+                                '計網中心_2F大門'	:'成功校區 計網中心',
+                                '計網中心_2F側門'	:'成功校區 計網中心',
+                                '計網中心_3F_75301'	:'成功校區 計網中心',
+                                '計網中心_3F_75309'	:'成功校區 計網中心',
+                                '計網中心_3F側門'	:'成功校區 計網中心',
+                                '計網中心_3F會議室'	:'成功校區 計網中心',
+                                '計網中心_4F大門'	:'成功校區 計網中心',
+                                '計網中心_5F大門':'成功校區 計網中心',
+                                '計網中心_GCU(16門）'	:'成功校區 計網中心',
+                                '計網中心6F大門' :'成功校區 計網中心'}
+    
         print('locations count:', len(self.location_names.keys()))
         # self.conn = sqlite3.connect(':memory:')
         # self.c = self.conn.cursor()
 
-    def get_date_data(self, place_name, start_date=datetime.date.today(), end_date=datetime.date.today()):
-        url_config = {
-            'place': place_name,
-            'startdate': start_date.strftime('%Y-%m-%d %H:%M:%S'),
-            'enddate': end_date.strftime('%Y-%m-%d %H:%M:%S')
-        }
-
-        url = eval(f"f'{self.url_template}'", url_config)
-        res = requests.get(url)
-        data = eval(res.content)
-
-        # # for debugging
-        # with open(f"./data/{url_config['place']}_{url_config['startdate']}_{url_config['enddate']}.json".replace(
-        #         ':', '-'), 'w') as fw:
-        #     json.dump(data, fw, indent=4)
-
-        return data
-
-    def get_relative_date_data(self, place_name, end_date, hours):
-        start_date = end_date - datetime.timedelta(hours=hours)
-        data = self.get_date_data(place_name, start_date, end_date)
-
-        return data
-
     def get_date_datas_fast(self, place_names, start_date=datetime.date.today(), end_date=datetime.date.today()):
-        """
-            place_name: 門機
-            start_date: 開始時間，預設今日00:00
-            end_date: 結束時間 ，預設今日00:00(需調整)
-
-            return
-            datas = 門機資料的list [門機1, 門機2,...]
-            response_list = response的list (檢查連線狀況)
-        """
+        # url_config = {
+        #     # 'place': place_name,
+        #     'startdate': start_date.strftime('%Y-%m-%d %H:%M:%S'),
+        #     'enddate': end_date.strftime('%Y-%m-%d %H:%M:%S')
+        # }
         urls = []
-        response_list = []
+        res_list = []
         for place_name in place_names:
             url_config = {
                 'place': place_name,
@@ -125,158 +126,83 @@ class CampusDataMonitor:
             }
             url = eval(f"f'{self.url_template}'", url_config)
             urls.append(url)
-            response_list.append(grequests.get(url))
+            res_list.append(grequests.get(url))
 
-        response_list = grequests.map(response_list)
-
-        # grequest 如果不能用，就用這個
-        # afr = ThreadRequest()
-        # res_list = afr.get_urls(urls)
-        ##
-
+        res_list = grequests.map(res_list)
+        #afr = ThreadRequest()
+        #res_list = afr.get_urls(urls)
         datas = []
-        for rs in response_list:
-            if rs is not None:
-                data = eval(rs.content)
+        for rs in res_list:
+            if rs == None:
+                data = None
             else:
-                print('request got none')
-                data = []
+                data = eval(rs.content)
             datas.append(data)
+        return datas, res_list
 
-        return datas, response_list
+    def get_relative_date_datas_fast(self, place_names, end_date, minutes):
+        start_date = end_date - datetime.timedelta(minutes=minutes)
+        datas, res_list = self.get_date_datas_fast(place_names, start_date, end_date)
 
-    def get_relative_date_datas_fast(self, place_names, end_date, hours):
-        """
-            取得從end_date往前hours時間段內的門機資料
-            end_date-hours ~ end_date
-            end_date: 結束時間
-            hours: 往前小時數
+        return datas, res_list
 
-            return
-            datas = 門機資料的list [門機1, 門機2,...]
-            response_list = response的list (檢查連線狀況)
-        """
-        start_date = end_date - datetime.timedelta(hours=hours)
-        datas, response_list = self.get_date_datas_fast(place_names, start_date, end_date)
-
-        return datas, response_list
-
-    @staticmethod
-    def time_interval_index(times, t_time):
-        for i in range(len(times) - 1):
-            if times[i] < t_time < times[i + 1]:
-                return i
-        return len(times) - 1
-
-    def divide_data_into_timegroups(self, data, start_time, interval=60, end_time=None):
-        """
-            把門機資料統計成以時段分割
-            Divide data into timegroups by given start_time, interval, end_time
-            times = [start_time, start_time+1*interval,..., start_time+n*interval]
-            start_time+n*interval >= end_time
-        """
-        if end_time is None:
-            end_time = datetime.datetime.combine(datetime.date.today(), datetime.time()) + datetime.timedelta(days=1)
-
-        if type(start_time) == datetime.date:
-            itime = datetime.datetime(start_time.year, start_time.month, start_time.day)
-        elif type(start_time) == datetime.datetime:
-            itime = start_time
+def Take_Data_Now(cdm):
+    t = time.time()
+    tonow = tonow = datetime.datetime.now()
+    minute_passed = tonow.hour*60 + tonow.minute
+    rdata, rres_list = cdm.get_relative_date_datas_fast(list(cdm.location_names.keys()), datetime.datetime.now(),
+                                                            minute_passed)
+    success = 0
+    fail = 0
+    for rres in rres_list:
+        if rres==None:
+            fail+=1
         else:
-            raise Exception("Wrong start_time type")
-
-        if type(interval) == int:
-            interval = datetime.timedelta(minutes=interval)
-
-        times = [itime]
-        groups = [[]]
-        group_sums = [0]
-        while itime < end_time:
-            # itime should >= end_time
-            # print(itime)
-            itime += interval
-            times.append(itime)
-            groups.append([])
-            group_sums.append(0)
-
-        for info in data:
-            # "t_time": "2021-10-21 13:27:20",
-            t_time = info.get('t_time')
-            if t_time is None:
-                continue
-
-            t_time = datetime.datetime.strptime(t_time, "%Y-%m-%d %H:%M:%S")
-            ind = self.time_interval_index(times, t_time)
-            if ind is None:
-                continue
-
-            groups[ind].append(info)
-            group_sums[ind] += 1
-
-        return groups, group_sums, times
-
-    def plot_animate(self, place):
-        plt.ion()
-        plt.rcParams['font.sans-serif'] = ['Microsoft JhengHei']
-        plt.rcParams['axes.unicode_minus'] = False
-        while True:
-            print('loop')
-            rdata = self.get_relative_date_data(place, datetime.datetime.now(), 12)
-            g, gs, ts = self.divide_data_into_timegroups(rdata, datetime.date.today())
-
-            gs = gs[:-1]
-            ts = ts[:-1]
-            ts_names = []
-            for t in ts:
-                ts_names.append(t.strftime('%H:%M'))
-            plt.plot(ts_names, gs)
-            for i, j in zip(ts_names, gs):
-                if j != 0:
-                    plt.annotate(str(j), xy=(i, j + 0.5))
-            plt.draw()
-            plt.suptitle(f'{place} 門禁刷卡統計')
-            plt.title(datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
-            plt.xlabel('時段')
-            plt.ylabel('刷卡人數')
-            # plt.pause(1)
-            time.sleep(3)
-            plt.clf()
-
-
-if __name__ == '__main__':
-    place = '計網中心_1F自動門_進'
-    cdm = CampusDataMonitor()
-    # cdm.plot_animate(place)
-    # exit()
-    cdm.get_date_data(place)
-    while True:
-        # rdata = cdm.get_relative_date_data(place, datetime.datetime.now(), 12)
-        rdata = cdm.get_relative_date_data(place, datetime.datetime.now(), 12)
-        g, gs, ts = cdm.divide_data_into_timegroups(rdata, datetime.date.today())
-
-        gs = gs[:-1]
-        ts = ts[:-1]
-        ts_names = []
-        for t in ts:
-            ts_names.append(t.strftime('%H:%M'))
-        # pprint.pp(list(zip(ts_names, gs)))
-        # cdm.plot_animate(place)
-
-        t = time.time()
-        rdata, rres_list = cdm.get_relative_date_datas_fast(list(cdm.location_names.keys()), datetime.datetime.now(),
-                                                            12)
-        success = 0
-        fail = 0
-        for rres in rres_list:
             if rres.status_code == 200:
                 success += 1
-            else:
+            else: 
                 fail += 1
-
-        print(f'success: {success}, failed: {fail}')
-        print(len(rdata))
-        print('time cost:', time.time() - t, '(s)')
-        rsum = []
-        for rd in rdata:
+    rsum = []
+    for rd in rdata:
+        if rd==None:
+            rsum.append(0)
+        else:
             rsum.append(len(rd))
-        # pprint.pprint(list(zip(list(cdm.location_names.keys()), rsum)))
+    #pprint.pprint(list(zip(list(cdm.location_names.keys()), rsum)))
+    """for i in range(len(rsum)):
+        if(rsum[i]>0):
+            print(list(cdm.location_names.keys())[i],' : ',rsum[i],'\n')"""
+    machine_name=[]
+    for i in range(len(rsum)):
+        machine_name.append(list(cdm.location_names.keys())[i])
+    dic={
+    "門機名稱" : machine_name,
+    "人數" : rsum
+    }
+    Data_dataframe=pd.DataFrame(dic)
+    location_names_path='./config/ogwebbasesetting_ogattlistctrl_ascx_211021.csv'
+    location_lng_lat='個場所對應經緯度.xlsx'
+    xlsx_data=pd.read_excel(location_lng_lat)
+    csv_data=pd.read_csv(location_names_path)
+    data=pd.DataFrame(csv_data)
+    location_data=pd.DataFrame(xlsx_data)
+    Data_handled=['光復校區 雲平大樓','成功校區 資訊工程系','光復校區 文學院修齊大樓','成功校區 計網中心']
+    n=[]
+    result=[['光復校區 雲平大樓',0],['成功校區 資訊工程系',0],['光復校區 文學院修齊大樓',0],['成功校區 計網中心',0]]
+    c=0
+    for i in range(len(Data_handled)):
+        n.append([0,0,0])
+    Location_dic=dict(zip(Data_handled,n))
+    #construc a dictionary of 管制區域:[人數,經度,緯度]
+    for i in range(len(rsum)):
+        for j in range(len(result)):
+            if(result[j][0]==list(data[(data['門名']==list(cdm.location_names.keys())[i])]['管制區域'])[0]):
+                result[j][1]+=rsum[i]
+    return result
+if __name__ == '__main__':
+    cdm = CampusDataMonitor()
+    while True:
+        result=Take_Data_Now(cdm)
+        print(result)
+        time.sleep(5)
+        
