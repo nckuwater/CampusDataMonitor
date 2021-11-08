e5514021d882b59fc551d1acc539c17ed9be6e50
adam <20120s49@gmail.com>
2021-11-08 16:20:13 +0800

    完成多線程

--- School_HeatMap.py
+++ School_HeatMap.py
@@ -21,12 +21,25 @@ import campus_data_monitor
 cdm =Take_data.CampusDataMonitor()
 cdm2 = campus_data_monitor.CampusDataMonitor()
 layout=QVBoxLayout()
+class TakeData_Thread(QThread):
+    trigger=pyqtSignal(list,list)
+    def __int__(self):
+        # 初始化函数
+        super(TakeData_Thread, self).__init__()
+    def run(self):
+        while(True):
+            time.sleep(10)
+            # Take data完傳送訊號給主線程
+            data =Take_data.Take_Data_Now(cdm)
+            data2 = campus_data_monitor.Take_Data_Now(cdm2)
+            self.trigger.emit(data,data2)
 class Window(QDialog):
     def __init__(self):
         QDialog.__init__(self)
         self.setWindowTitle("People_Heat_Map")
         self.resize(640, 480)
         self.setLayout(layout)
+
         data =Take_data.Take_Data_Now(cdm)
         data2 = campus_data_monitor.Take_Data_Now(cdm2)
         fmap = folium.Map(location=[22.996363580424514, 120.21903238440177], zoom_start=17)
@@ -80,19 +93,21 @@ class Window(QDialog):
         webView=QtWebEngineWidgets.QWebEngineView()
         webView.setHtml(data2.getvalue().decode())
         layout.addWidget(webView)
-        self.Update()
-        QtWidgets.QApplication.processEvents()
 
-    def Update(self):
-        self.timer=QTimer()
-        self.timer.timeout.connect(self.run)
-        self.timer.start(15000)
+        #setup Thread***************************************************
+        self.work=TakeData_Thread()
+        self.work.start()
+        self.work.trigger.connect(self.Display)
+        #****************************************************************
         QtWidgets.QApplication.processEvents()
-    def run(self):
-        Data =Take_data.Take_Data_Now(cdm)
-        data2 = campus_data_monitor.Take_Data_Now(cdm2)
-        
+    def Display(self,data,data2):
+        layout.takeAt(0).widget().deleteLater()
         fmap = folium.Map(location=[22.996363580424514, 120.21903238440177], zoom_start=17)
+        HeatMap(data,min_opacity=0.4,blur=50,radius=50).add_to(fmap)
+        fmap.save("test.html")
+        #folium.Marker(location=[22.99891110398634, 120.21723168980603],popup='<i>雲平大樓：<i>').add_to(fmap)
+
+
         html =   '''<font face="微軟正黑體"><font size="4">雲平大樓:</font>
                     ''' + str(data2[0][1])
         iframe = folium.IFrame(html,width=150,height=40)
@@ -114,6 +129,7 @@ class Window(QDialog):
         iframe = folium.IFrame(html,width=150,height=40)
         popup = folium.Popup(iframe,show=True,max_width=200,max_height=200)
         m3 = folium.Marker(location=[22.99811369711759, 120.2184468855774],popup=popup)
+
         html =   '''<font face="微軟正黑體"><font size="4">光復機車:</font>
                     ''' + str(data2[4][1])
         iframe = folium.IFrame(html,width=160,height=40)
@@ -124,18 +140,18 @@ class Window(QDialog):
         iframe = folium.IFrame(html,width=150,height=40)
         popup = folium.Popup(iframe,show=True,max_width=200,max_height=200)
         m5 = folium.Marker(location=[22.999008047465647, 120.21451704980048],popup=popup)
+
         fmap.add_child(child=m0)
         fmap.add_child(child=m1)
         fmap.add_child(child=m2)
         fmap.add_child(child=m3)
         fmap.add_child(child=m4)
         fmap.add_child(child=m5)
-        HeatMap(Data,min_opacity=0.4,blur=50,radius=50).add_to(fmap)
+
         data2 = io.BytesIO()
         fmap.save(data2,close_file=False)
         webView=QtWebEngineWidgets.QWebEngineView()
         webView.setHtml(data2.getvalue().decode())
-        layout.takeAt(0).widget().deleteLater()
         layout.addWidget(webView)
         QtWidgets.QApplication.processEvents()
         
